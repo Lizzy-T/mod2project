be2b85ebde9be9cb7f133c272f4abbe31299f8f6
Damon Self <damongself@gmail.com>
2019-09-30 13:16:23 -0600

    Adds create to users

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -10,4 +10,12 @@ class UsersController < ApplicationController
 
     render json: @user
   end
+
+  def create
+    @user = User.create({
+      username: params[:username]
+    })
+
+    render json: @user
+  end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :favorites, only: [:index, :show]
   resources :reihikes, only: [:index, :show]
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
