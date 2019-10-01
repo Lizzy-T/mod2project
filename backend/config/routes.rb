06b84818b91dde4a086da0024045bd057aba0baa
Damon Self <damongself@gmail.com>
2019-10-01 10:54:08 -0600

    Fixed period-comma issue in routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :favorites, only: [:index, :show. :create]
+  resources :favorites, only: [:index, :show, :create]
   resources :reihikes, only: [:index, :show]
   resources :users, only: [:index, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
